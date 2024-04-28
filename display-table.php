90491641b13c85010a53af28360868cf042fbcb3
sarthaknp977 <sarthaknp977@gmail.com>
2024-04-28 08:59:38 +0545

    Implemented option to view all data of a table

--- display-table.php
+++ display-table.php
@@ -1,31 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-try {
-    $serverName = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "db_13";
-    $connection = mysqli_connect($serverName, $username, $password, $dbname);
-    $selectQuery = "SELECT * FROM books_info";
-    $result = mysqli_query($connection, $selectQuery);
-    if (mysqli_num_rows($result) > 0) {
-        echo "<h2>Book List</h2>";
-        echo "<table border='1'>";
-        echo "<tr><th>Title</th><th>Book Code</th><th>Author</th><th>Price</th><th>Page</th><th>Stock</th></tr>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['title'] . "</td>";
-            echo "<td>" . $row['book_code'] . "</td>";
-            echo "<td>" . $row['author'] . "</td>";
-            echo "<td>" . $row['price'] . "</td>";
-            echo "<td>" . $row['page'] . "</td>";
-            echo "<td>" . $row['stock'] . "</td>";
-            echo "</tr>";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style-table.css">
+</head>
+
+<body>
+    <?php
+
+    try {
+        $serverName = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "db_13";
+        $connection = mysqli_connect($serverName, $username, $password, $dbname);
+        $selectQuery = "SELECT * FROM books_info";
+        $result = mysqli_query($connection, $selectQuery);
+        if (mysqli_num_rows($result) > 0) {
+            echo '<div class="table-container">';
+            echo "<h2>Book List</h2>";
+            echo "<table class='table'>";
+            echo "<tr><th>Book Code</th><th>Title</th><th>Author</th><th>Price(in $)</th><th>Page</th><th>Stock</th></tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['book_code'] . "</td>";
+                echo "<td>" . $row['title'] . "</td>";
+                echo "<td>" . $row['author'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['page'] . "</td>";
+                echo "<td>" . $row['stock'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            echo '</div';
+        } else {
+            echo "No records found.";
         }
-        echo "</table>";
-    } else {
-        echo "No records found.";
+    } catch (Exception $exception) {
+        echo "Database Error: " . $exception->getMessage();
     }
-} catch (Exception $exception) {
-    echo "Database Error: " . $exception->getMessage();
-}
+    ?>
+</body>
+
+</html>
